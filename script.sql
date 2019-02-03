54a827872e1e5349ebb4551411480270a9fdb3c8
dxvo <dxvo@uh.edu>
2019-02-03 01:07:12 -0600

    update

--- script.sql
+++ script.sql
@@ -147,21 +147,33 @@ GROUP BY P.customer_id
 ORDER BY last_name; 
 
 
+#7a. 
+-- The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
+-- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
+-- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+SELECT film.title
+FROM film 
+WHERE  ((title LIKE 'K%' ) OR (title LIKE 'Q%' )) AND language_id IN 
+(
+	SELECT language_id
+    FROM language
+    WHERE name = 'English'
+); 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#7b
+-- Use subqueries to display all actors who appear in the film Alone Trip.
+SELECT first_name, last_name
+FROM actor 
+WHERE actor_id IN 
+(
+	SELECT actor_id
+    FROM film_actor
+    WHERE film_id IN 
+    (
+		SELECT film_id
+		FROM film
+		WHERE title = 'Alone Trip'));
 
 
 
